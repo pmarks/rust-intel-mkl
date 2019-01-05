de769f6700fc0b8e3703d12ba5c6f43189498184
Patrick Marks <pmarks@gmail.com>
2019-01-05 09:18:24 -0800

    convert build to use Anaconda artifacts for MKL

--- build.rs
+++ build.rs
@@ -21,37 +21,65 @@
 // SOFTWARE.
 
 extern crate crypto;
+extern crate curl;
+extern crate bzip2;
+extern crate tar;
 
 use crypto::md5;
 use crypto::digest::Digest;
 
+use curl::easy::Easy;
+use bzip2::read::BzDecoder;
+use tar::Archive;
+
 use std::env::var;
 use std::path::*;
-use std::process::Command;
-use std::fs;
+use std::fs::{self, File};
 use std::io::*;
 
 #[cfg(target_os = "linux")]
 mod mkl {
     pub const ARCHIVE: &'static str = "mkl_linux.tar.xz";
-    pub const MD5SUM: &'static str = "03aa6b3918da6046b1225aacd244363a";
-    pub const URI: &'static str = "https://www.dropbox.com/s/nnlfdio0ka9yeo1/mkl_linux.tar.xz";
+    pub const MD5SUM: &'static str = "9c6c0d8609ffb94af492d0c07fb7f22c";
+    pub const URI: &'static str = "https://anaconda.org/anaconda/mkl/2019.1/download/linux-64/mkl-2019.1-144.tar.bz2";
+    pub const SO_PATH: &'static str = "lib";
 }
 
 #[cfg(target_os = "macos")]
 mod mkl {
     pub const ARCHIVE: &'static str = "mkl_osx.tar.xz";
-    pub const MD5SUM: &'static str = "3774e0c8b4ebcb8639a4f293d749bd32";
-    pub const URI: &'static str = "https://www.dropbox.com/s/fw74msh8udjdv28/mkl_osx.tar.xz";
+    pub const MD5SUM: &'static str = "c9598d7fb664a16f0abfc2cb71491b62";
+    pub const URI: &'static str = "https://anaconda.org/anaconda/mkl/2019.1/download/osx-64/mkl-2019.1-144.tar.bz2";
+    pub const SO_PATH: &'static str = "lib";
+}
+
+#[cfg(target_os = "windows")]
+mod mkl {
+    pub const ARCHIVE: &'static str = "mkl_win.tar.xz";
+    pub const MD5SUM: &'static str = "429418273137ee591a55a0a210cf7436";
+    pub const URI: &'static str = "https://anaconda.org/anaconda/mkl/2019.1/download/win-64/mkl-2019.1-144.tar.bz2";
+    pub const SO_PATH: &'static str = "Library/bin";
 }
 
 fn download(uri: &str, filename: &str, out_dir: &Path) {
-    let out = out_dir.join(filename);
-    let mut f = BufWriter::new(fs::File::create(out).unwrap());
-    let p = Command::new("curl").args(&["-L", uri]).output().expect(
-        "Failed to start download",
-    );
-    f.write(&p.stdout).unwrap();
+
+    let out = PathBuf::from(out_dir.join(filename));
+
+    // Download the tarball.
+    let f = File::create(&out).unwrap();
+    let mut writer = BufWriter::new(f);
+    let mut easy = Easy::new();
+    easy.follow_location(true).unwrap();
+    easy.url(&uri).unwrap();
+    easy.write_function(move |data| {
+        Ok(writer.write(data).unwrap())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    let response_code = easy.response_code().unwrap();
+    if response_code != 200 {
+        panic!("Unexpected response code {} for {}", response_code, uri);
+    }
 }
 
 fn calc_md5(path: &Path) -> String {
@@ -63,15 +91,11 @@ fn calc_md5(path: &Path) -> String {
     sum.result_str()
 }
 
-fn expand(archive: &Path, out_dir: &Path) {
-    let st = Command::new("tar")
-        .args(&["xvf", archive.to_str().unwrap()])
-        .current_dir(&out_dir)
-        .status()
-        .expect("Failed to start expanding archive");
-    if !st.success() {
-        panic!("Failed to expand archive");
-    }
+fn extract<P: AsRef<Path>, P2: AsRef<Path>>(archive_path: P, extract_to: P2) {
+    let file = File::open(archive_path).unwrap();
+    let unzipped = BzDecoder::new(file);
+    let mut a = Archive::new(unzipped);
+    a.unpack(extract_to).unwrap();
 }
 
 fn main() {
@@ -81,8 +105,9 @@ fn main() {
     if archive_path.exists() && calc_md5(&archive_path) == mkl::MD5SUM {
         println!("Use existings archive");
     } else {
-        println!("Downlaod archive");
+        println!("Download archive");
         download(mkl::URI, mkl::ARCHIVE, &out_dir);
+        extract(&archive_path, &out_dir);
         let sum = calc_md5(&archive_path);
         if sum != mkl::MD5SUM {
             panic!(
@@ -91,12 +116,10 @@ fn main() {
             );
         }
     }
-    expand(&archive_path, &out_dir);
-
-    println!("cargo:rustc-link-search={}", out_dir.display());
+    
+    println!("cargo:rustc-link-search={}", out_dir.join(mkl::SO_PATH).display());
     println!("cargo:rustc-link-lib=mkl_intel_lp64");
-    println!("cargo:rustc-link-lib=mkl_intel_thread");
+    println!("cargo:rustc-link-lib=mkl_sequential");
     println!("cargo:rustc-link-lib=mkl_core");
-    println!("cargo:rustc-link-lib=iomp5");
     println!("cargo:rustc-link-lib=m");
 }
